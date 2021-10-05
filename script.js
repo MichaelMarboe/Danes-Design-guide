d2ef3182d228f03e4a91972ab2baae528d38cec3
Michael Marboe <michael@Michaels-iMac.local>
2021-10-05 12:41:24 +0200

    fix typos, change font to wave, add click effect to color box copy function

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 function copyText(hexCode) {
-    navigator.clipboard.writeText(hexCode);
-  }
+  navigator.clipboard.writeText(hexCode);
+};
+
+/*
+  Color-event: Must be handled in for loop because .getElementsByClassName returns a htmlcollection and not an array (so .forEach won't work).
+  Opacity used instead of backgroundColor to produce color change effect. This makes it possible to avoid having to save colors as variables.
+  mouseenter and mouseleave event is only there to prevent holding mousedown, leaving object and then altering color permanently. 
+*/
+
+const colorSquares = document.getElementsByClassName("colorSquare");
+for (let i = 0; i < colorSquares.length; i++) {
+  colorSquares[i].addEventListener("mouseup", function () {
+    colorSquares[i].style.opacity = "1";
+  });
+  colorSquares[i].addEventListener("mousedown", function () {
+    colorSquares[i].style.opacity = "0.5";
+  });
+  colorSquares[i].addEventListener("mouseenter", function () {
+    colorSquares[i].style.opacity = "0.8";
+  });
+  colorSquares[i].addEventListener("mouseleave", function () {
+    colorSquares[i].style.opacity = "1";
+  });
+}
+
+
